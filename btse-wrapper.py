ceea48d6191a772ec7ec3c29b317276696b8f98e
Richard Hogan <36215881+hoganri@users.noreply.github.com>
2019-07-26 14:08:07 -0400

    Update btse-wrapper.py

--- btse-wrapper.py
+++ btse-wrapper.py
@@ -3,9 +3,6 @@ import requests, json
 # BTSE API Wrapper
 # Start building or migrate your trading
 # algorithms to the BTSE platform
-#
-# Tipjar: bc1q3taedwlg6955eda6e40k7uaxrvfmtrx9apjsw3
-# Referral URL: https://www.btse.com/en/home?ref=7MnXDG
 
 # ****************************************** #
 #
